12495e1efb6ebc4da1a1b1f60fcefb16f2dc9f3d
Alberto AAR <qmvc.tel@gmail.com>
2017-01-11 14:48:44 -0600

    Use ArrayList intead of Array

--- app/src/main/java/com/example/goku/miwokudacityapp/NumbersActivity.java
+++ app/src/main/java/com/example/goku/miwokudacityapp/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.goku.miwokudacityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.v("NumberActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumberActivity", "Word at index 1: " + words.get(1));
+        Log.v("NumberActivity", "Word at index 2: " + words.get(2));
+        Log.v("NumberActivity", "Word at index 3: " + words.get(3));
+        Log.v("NumberActivity", "Word at index 4: " + words.get(4));
+        Log.v("NumberActivity", "Word at index 5: " + words.get(5));
+        Log.v("NumberActivity", "Word at index 6: " + words.get(6));
+        Log.v("NumberActivity", "Word at index 7: " + words.get(7));
+        Log.v("NumberActivity", "Word at index 8: " + words.get(8));
+        Log.v("NumberActivity", "Word at index 9: " + words.get(9));
+
     }
 }
