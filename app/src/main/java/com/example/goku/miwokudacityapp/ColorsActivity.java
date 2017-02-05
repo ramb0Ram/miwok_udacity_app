a84d40d0ce3d1f89f17ff30254491cec3d9f1962
Alberto AAR <qmvc.tel@gmail.com>
2017-02-04 20:31:22 -0600

    ColorsActivity

--- app/src/main/java/com/example/goku/miwokudacityapp/ColorsActivity.java
+++ app/src/main/java/com/example/goku/miwokudacityapp/ColorsActivity.java
@@ -2,6 +2,9 @@ package com.example.goku.miwokudacityapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        WordAdapter listAdapter = new WordAdapter(this, words);
+        ListView listItem = (ListView)findViewById(R.id.list_colors);
+        listItem.setAdapter(listAdapter);
     }
 }
